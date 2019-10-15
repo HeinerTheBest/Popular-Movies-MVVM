c71d4083c7a18eb0132074fec2fd528ab2af3340
Admin <heinerbauza@gmail.com>
2019-10-15 02:22:11 -0400

    *Finish API call

--- app/src/main/java/com/mobileapps/popularmoviesmvvm/model/datasource/remote/MovieDbApi.kt
+++ app/src/main/java/com/mobileapps/popularmoviesmvvm/model/datasource/remote/MovieDbApi.kt
@@ -1,6 +1,17 @@
 package com.mobileapps.popularmoviesmvvm.model.datasource.remote
 
+import com.mobileapps.popularmoviesmvvm.model.datasource.remote.MovieDbApiHelper.DB_MOVIE_DETAIL_PATH
+import com.mobileapps.popularmoviesmvvm.model.datasource.remote.MovieDbApiHelper.DB_POPULAR_MOVIES_PATH
+import com.mobileapps.popularmoviesmvvm.model.themoviedb.MovieDbResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface MovieDbApi
 {
+    @GET(DB_POPULAR_MOVIES_PATH)
+    fun getPopularMovies() : Call<MovieDbResponse>
 
+    @GET(DB_MOVIE_DETAIL_PATH)
+    fun getMovieDetail(@Path("movieId")id : Int) : Call<MovieDbResponse>
 }
